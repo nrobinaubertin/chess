4251b61fb1adb4b223233f80aa6f1ad2092a6803
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-16 10:04:59 +0200

    Prepare chess.c to make benchmarks

--- chess.c
+++ chess.c
@@ -12,26 +12,73 @@
 #include "print.h"
 #include "move.h"
 
+int is_game_over(board b) {
+    bool white = false;
+    bool black = false;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            int k = 21 + j + 10*i;
+            if (b->piece[k] == 6) {
+                if (b->color[k] == 1) {
+                    white = true;
+                } else {
+                    black = true;
+                }
+            }
+            if (white && black) {
+                return 0;
+            }
+        }
+    }
+    if (white)
+        return 1;
+    return -1;
+}
+
 int main(int argc, char* argv[]) {
 
-    srand((unsigned) time(NULL));
+    int max_t = 0;
+    int seed = 0;
 
-    board b = create_board();
-    assert(b);
-    b = init_board(b);
-    print_board(b);
+    for(int s = 0; s < 100; s++) {
 
-    int color = 1;
-    for(int i = 0; i < 100; i++) {
-        move m = rand_move(b, color);
-        printf("\n");
-        print_move(m);
-        printf("\n");
-        apply_move(m, b);
-        print_board(b);
-        sleep(4);
-        color *= -1;
+        srand(s);
+        // srand((unsigned) time(NULL));
+
+        board b = create_board();
+        assert(b);
+        b = init_board(b);
+        //print_board(b);
+
+        int color = 1;
+        int w = 0;
+        int turn = 0;
+        while(! (w = is_game_over(b))) {
+            move m = rand_move(b, color);
+            // printf("\n");
+            // print_move(m);
+            // printf("\n");
+            apply_move(m, b);
+            // print_board(b);
+            // sleep(2);
+            color *= -1;
+            turn++;
+            free(m);
+        }
+
+        if (turn > max_t) {
+            max_t = turn;
+            seed = s;
+        }
+        destroy_board(b);
+    // if (w == 1) {
+    //     printf("White wins !\n");
+    // } else {
+    //     printf("Black wins !\n");
+    // }
+    // printf("The play lasted %d turns.\n", turn);
     }
+    printf("%d %d\n", seed, max_t);
 
     // char coord[3];
     // int k;
@@ -44,6 +91,5 @@ int main(int argc, char* argv[]) {
     //     printf("\n");
     // }
 
-    destroy_board(b);
     return EXIT_SUCCESS;
 }
