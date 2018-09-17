d218ac1986137e1f7b249f84a7f69b7fbe25a354
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-17 09:00:11 +0200

    Fix and improve speed of evaluate function

--- evaluate.c
+++ evaluate.c
@@ -2,7 +2,6 @@
 #include <assert.h>
 #include "evaluate.h"
 
-
 // Piece-Square Tables
 
 const int eval_pawn[120] = {
@@ -85,60 +84,58 @@ int evaluate(board b, int color) {
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             int k = 21 + j + 10*i;
-            int kk = 21 + j + 70 - 10*i; // inverted k (for black piece square tables)
-            if (b->piece[k] != 7) {
-                switch (b->piece[k]) {
-                    case 1:
-                        // doubled or blocked pawns
-                        if (b->piece[k + 10] == 1) {
-                            score += b->color[k] * 50;
-                        } else {
-                            score += b->color[k] * 100;
-                        }
-                        if (b->color[k] == 1) {
-                            score += eval_pawn[k];
-                        } else {
-                            score += eval_pawn[kk];
-                        }
-                        break;
-                    case 2:
-                        score += b->color[k] * 300;
-                        if (b->color[k] == 1) {
-                            score += eval_knight[k];
-                        } else {
-                            score += eval_knight[kk];
-                        }
-                        break;
-                    case 3:
-                        if (b->color[k] == 1) {
-                            score += eval_bishop[k];
-                        } else {
-                            score += eval_bishop[kk];
-                        }
-                        score += b->color[k] * 325;
-                        break;
-                    case 4:
-                        score += b->color[k] * 500;
-                        if (b->color[k] == 1) {
-                            score += eval_rook[k];
-                        } else {
-                            score += eval_rook[kk];
-                        }
-                        break;
-                    case 5:
-                        score += b->color[k] * 900;
-                        if (b->color[k] == 1) {
-                            score += eval_queen[k];
-                        } else {
-                            score += eval_queen[kk];
-                        }
-                        break;
-                    case 6:
-                        score += b->color[k] * 20000;
-                        break;
-                    default:
-                        break;
-                }
+            switch (b->piece[k]) {
+                case 1:
+                    // doubled or blocked pawns
+                    if (b->piece[k + 10 * color] == 1) {
+                        score += b->color[k] * 50;
+                    } else {
+                        score += b->color[k] * 100;
+                    }
+                    if (b->color[k] == 1) {
+                        score += eval_pawn[k];
+                    } else {
+                        int kk = 21 + j + 70 - 10*i; // inverted k (for black piece square tables)
+                        score += eval_pawn[kk];
+                    }
+                    break;
+                case 2:
+                    if (b->color[k] == 1) {
+                        score += eval_knight[k] + 300;
+                    } else {
+                        int kk = 21 + j + 70 - 10*i; // inverted k (for black piece square tables)
+                        score += eval_knight[kk] - 300;
+                    }
+                    break;
+                case 3:
+                    if (b->color[k] == 1) {
+                        score += eval_bishop[k] + 325;
+                    } else {
+                        int kk = 21 + j + 70 - 10*i; // inverted k (for black piece square tables)
+                        score += eval_bishop[kk] - 325;
+                    }
+                    break;
+                case 4:
+                    if (b->color[k] == 1) {
+                        score += eval_rook[k] + 500;
+                    } else {
+                        int kk = 21 + j + 70 - 10*i; // inverted k (for black piece square tables)
+                        score += eval_rook[kk] - 500;
+                    }
+                    break;
+                case 5:
+                    if (b->color[k] == 1) {
+                        score += eval_queen[k] + 900;
+                    } else {
+                        int kk = 21 + j + 70 - 10*i; // inverted k (for black piece square tables)
+                        score += eval_queen[kk] - 900;
+                    }
+                    break;
+                case 6:
+                    score += b->color[k] * 20000;
+                    break;
+                default:
+                    break;
             }
         }
     }
